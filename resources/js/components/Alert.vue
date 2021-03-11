ef4d9522d4387bce4654cc6db9142e346b0c88fe
Grant Holle <hollegrant@gmail.com>
2021-03-11 14:14:04 +0800

    Update alerts for dark theme

--- resources/js/components/Alert.vue
+++ resources/js/components/Alert.vue
@@ -37,28 +37,28 @@ export default defineComponent({
   setup ({ level }) {
     let show = ref(true)
     const bgColors = {
-      success: 'bg-green-50',
-      error: 'bg-red-50',
-      warning: 'bg-yellow-50',
-      neutral: 'bg-gray-50',
+      success: 'bg-green-50 dark:bg-green-800',
+      error: 'bg-red-50 dark:bg-red-800',
+      warning: 'bg-yellow-50 dark:bg-yellow-800',
+      neutral: 'bg-gray-50 dark:bg-gray-700',
     }
     const iconColors = {
-      success: 'text-green-400',
-      error: 'text-red-400',
-      warning: 'text-yellow-400',
-      neutral: 'text-gray-400',
+      success: 'text-green-400 dark:text-green-200',
+      error: 'text-red-400 dark:text-red-200',
+      warning: 'text-yellow-400 dark:text-yellow-200',
+      neutral: 'text-gray-400 dark:text-gray-200',
     }
     const textColors = {
-      success: 'text-green-800',
-      error: 'text-red-800',
-      warning: 'text-yellow-800',
-      neutral: 'text-gray-800',
+      success: 'text-green-800 dark:text-green-100',
+      error: 'text-red-800 dark:text-red-100',
+      warning: 'text-yellow-800 dark:text-yellow-100',
+      neutral: 'text-gray-800 dark:text-gray-100',
     }
     const closeColors = {
-      success: 'bg-green-50 text-green-500 hover:bg-green-100 focus:ring-offset-green-50 focus:ring-green-600',
-      error: 'bg-red-50 text-red-500 hover:bg-red-100 focus:ring-offset-red-50 focus:ring-red-600',
-      warning: 'bg-yellow-50 text-yellow-500 hover:bg-yellow-100 focus:ring-offset-yellow-50 focus:ring-yellow-600',
-      neutral: 'bg-gray-50 text-gray-500 hover:bg-gray-100 focus:ring-offset-gray-50 focus:ring-gray-600',
+      success: 'bg-green-50 text-green-500 hover:bg-green-100 focus:ring-offset-green-50 focus:ring-green-600 dark:bg-green-800 dark:text-green-400',
+      error: 'bg-red-50 text-red-500 hover:bg-red-100 focus:ring-offset-red-50 focus:ring-red-600 dark:bg-red-800 dark:text-red-400',
+      warning: 'bg-yellow-50 text-yellow-500 hover:bg-yellow-100 focus:ring-offset-yellow-50 focus:ring-yellow-600 dark:bg-yellow-800 dark:text-yellow-400',
+      neutral: 'bg-gray-50 text-gray-500 hover:bg-gray-100 focus:ring-offset-gray-50 focus:ring-gray-600 dark:bg-gray-700 dark:text-gray-400',
     }
     const icons = {
       success: `<path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />`,
