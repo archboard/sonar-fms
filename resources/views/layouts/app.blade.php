c44fe002f5f0e1f07a6a24ee20e307458ec9ed9c
Grant Holle <hollegrant@gmail.com>
2021-03-11 09:08:46 +0800

    Add dark mode toggle functionality

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,13 +9,26 @@
   @else
     <title>{{ config('app.name') }}</title>
   @endisset
+    <script>
+      window.changeTheme = function (dark) {
+        if (dark) {
+          document.documentElement.classList.add('dark')
+        } else {
+          document.documentElement.classList.remove('dark')
+        }
+
+        localStorage.theme = dark ? 'dark' : 'light'
+      }
+
+      window.changeTheme(localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches))
+    </script>
 
     @routes
 
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
     <script src="{{ mix('/js/app.js') }}" defer></script>
   </head>
-  <body>
+  <body class="text-gray-900 dark:text-gray-100 bg-white dark:bg-gray-900">
     @inertia
   </body>
 </html>
