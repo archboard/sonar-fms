326795ebafae565fc6a5ce6df199477c763ab479
Grant Holle <hollegrant@gmail.com>
2021-03-11 13:08:39 +0800

    Automatically push flash messages

--- resources/js/plugins/http.js
+++ resources/js/plugins/http.js
@@ -1,7 +1,27 @@
 import axios from 'axios'
+import get from 'lodash/get'
+import store from '@/stores/notifications'
+
+axios.interceptors.response.use(response => {
+  const flash = get(response, 'data.props.flash')
+
+  if (flash) {
+    Object.keys(flash).forEach(level => {
+      const text = flash[level]
+
+      if (text) {
+        console.log(`${level}: ${flash[level]}`)
+        store.addNotification({ level, text })
+      }
+    })
+  }
+
+  return response
+})
 
 export default {
   install (app) {
     app.config.globalProperties.$http = axios
+    app.provide('$http', axios)
   }
 }
