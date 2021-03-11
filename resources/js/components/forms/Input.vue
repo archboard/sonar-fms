e8ea8382354160837ce0069450abb937ab60005f
Grant Holle <hollegrant@gmail.com>
2021-03-11 08:38:16 +0800

    Rename cyan to be primary

--- resources/js/components/forms/Input.vue
+++ resources/js/components/forms/Input.vue
@@ -3,7 +3,7 @@
     :type="type"
     :value="modelValue"
     @input="$emit('update:modelValue', $event.target.value)"
-    class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+    class="shadow-sm focus:ring-primary-500 focus:border-primary-500 block w-full sm:text-sm border-gray-300 rounded-md"
   >
 </template>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,10 @@ const colors = require('tailwindcss/colors')
 
 module.exports = {
   purge: [],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {
-      cyan: colors.cyan
+      primary: colors.cyan
     },
   },
   variants: {
