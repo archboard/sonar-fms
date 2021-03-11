5dd73fa1fa3be32e37c58f0e1853274b009d3c1a
Grant Holle <hollegrant@gmail.com>
2021-03-11 13:09:01 +0800

    Make simple tenant resource

--- app/Http/Resources/TenantResource.php
+++ app/Http/Resources/TenantResource.php
@@ -14,6 +14,10 @@ class TenantResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'domain' => $this->domain,
+            'license' => $this->license,
+            'subscription_started_at' => $this->subscription_started_at,
+        ];
     }
 }
