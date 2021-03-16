7e58044742f8fa7fb43327c0e3266c374b2e7ab6
Grant Holle <hollegrant@gmail.com>
2021-03-16 10:05:47 +0800

    Style password reset page

--- resources/js/pages/Auth/ResetPassword.vue
+++ resources/js/pages/Auth/ResetPassword.vue
@@ -1,30 +1,32 @@
 <template>
-  <ValidationErrors class="mb-4" />
+  <Layout>
+    <ValidationErrors class="mb-4" />
 
-  <form @submit.prevent="submit">
-    <Fieldset>
-      <InputWrap :error="form.errors.email">
-        <Label for="email">{{ __('Email') }}</Label>
-        <Input id="email" type="email" v-model="form.email" required autofocus autocomplete="username" />
-      </InputWrap>
+    <form @submit.prevent="submit">
+      <Fieldset>
+        <InputWrap :error="form.errors.email">
+          <Label for="email" class="text-base">{{ __('Email') }}</Label>
+          <Input id="email" type="email" class="text-lg" v-model="form.email" required autofocus autocomplete="username" />
+        </InputWrap>
 
-      <InputWrap :error="form.errors.password">
-        <Label for="password">{{ __('Password') }}</Label>
-        <Input id="password" type="password" v-model="form.password" required autocomplete="new-password" />
-      </InputWrap>
+        <InputWrap :error="form.errors.password">
+          <Label for="password" class="text-base">{{ __('Password') }}</Label>
+          <Input id="password" type="password" class="text-lg" v-model="form.password" required autocomplete="new-password" />
+        </InputWrap>
 
-      <InputWrap>
-        <Label for="password_confirmation">{{ __('Confirm Password') }}</Label>
-        <Input id="password_confirmation" type="password" v-model="form.password_confirmation" required autocomplete="new-password" />
-      </InputWrap>
-    </Fieldset>
+        <InputWrap>
+          <Label for="password_confirmation" class="text-base">{{ __('Confirm Password') }}</Label>
+          <Input id="password_confirmation" type="password" class="text-lg" v-model="form.password_confirmation" required autocomplete="new-password" />
+        </InputWrap>
+      </Fieldset>
 
-    <div class="flex items-center justify-end mt-4">
-      <Button :loading="form.processing">
-        Reset Password
-      </Button>
-    </div>
-  </form>
+      <div class="mt-6">
+        <Button :loading="form.processing" :is-block="true" size="lg">
+          {{ __('Reset Password') }}
+        </Button>
+      </div>
+    </form>
+  </Layout>
 </template>
 
 <script>
@@ -34,9 +36,11 @@ import Label from '@/components/forms/Label'
 import Input from '@/components/forms/Input'
 import Button from '@/components/Button'
 import ValidationErrors from '@/components/ValidationErrors'
+import Layout from '../../layouts/Guest'
 
 export default {
   components: {
+    Layout,
     ValidationErrors,
     Button,
     Input,
