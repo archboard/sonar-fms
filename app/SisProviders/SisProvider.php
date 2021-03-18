91dc6df821aa9b05ca345b6cf2c3b410eadd6107
Grant Holle <hollegrant@gmail.com>
2021-03-18 10:44:10 +0800

    Add missing functions to interface

--- app/SisProviders/SisProvider.php
+++ app/SisProviders/SisProvider.php
@@ -4,19 +4,23 @@ namespace App\SisProviders;
 
 use App\Models\School;
 use App\Models\Tenant;
+use GrantHolle\PowerSchool\Api\RequestBuilder;
 use Illuminate\Support\Collection;
 
 interface SisProvider
 {
     public function __construct(Tenant $tenant);
-
+    public function getSisLabel(): string;
     public function getAllSchools(): array;
-
     public function syncSchools(): Collection;
-
     public function getSchool($sisId);
-
     public function syncSchool($sisId): School;
-
+    public function syncSchoolStaff($sisId);
+    public function syncSchoolStudents($sisId);
     public function syncSchoolCourses($sisId);
+    public function syncSchoolSections($sisId);
+    public function syncSchoolStudentEnrollment($sisId);
+    public function syncSchoolTerms($sisId);
+    public function fullSchoolSync($sisId);
+    public function getBuilder(): RequestBuilder;
 }
