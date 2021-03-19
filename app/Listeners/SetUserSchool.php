c8712310914a0465b4c0d80840e0be0855cbb4a3
Grant Holle <hollegrant@gmail.com>
2021-03-19 13:18:09 +0800

    Set the users school if there isn't one set

---
+++ app/Listeners/SetUserSchool.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Models\User;
+use Illuminate\Auth\Events\Login;
+
+class SetUserSchool
+{
+    /**
+     * Handle the event.
+     *
+     * @param Login $event
+     * @return void
+     */
+    public function handle(Login $event)
+    {
+        /** @var User $user */
+        $user = $event->user;
+
+        if (!$user->school_id) {
+            $school = $user->schools->first();
+
+            if (!$school) {
+                throw new \Exception("You do not have any schools configured for your account. Please contact your district admin for help.");
+            }
+
+            $user->update(['school_id' => $school->id]);
+        }
+    }
+}
