6cc3d85e9fcddab09e7cf9bfc7980e48289ed0ab
Grant Holle <hollegrant@gmail.com>
2021-03-19 10:45:18 +0800

    Implement mobile nav

--- resources/js/layouts/Authenticated.vue
+++ resources/js/layouts/Authenticated.vue
@@ -40,55 +40,55 @@
               <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-logo-indigo-300-mark-white-text.svg" alt="Workflow">
             </div>
             <div class="mt-5 flex-1 h-0 overflow-y-auto">
-              <nav class="px-2 space-y-1">
-                <!-- Current: "bg-primary-900 dark:bg-primary-600 text-white", Default: "text-primary-100 hover:bg-primary-800 dark:hover:bg-primary-600" -->
-                <a href="#" class="bg-primary-900 dark:bg-primary-600 text-white text-white group flex items-center px-2 py-2 text-base font-medium rounded-md transition">
-                  <!-- Current: "text-primary-100", Default: "text-primary-300 group-hover:text-primary-200" -->
-                  <svg class="text-primary-100 mr-4 h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
-                  </svg>
-                  Dashboard
-                </a>
+              <nav class="px-2 space-y-6">
+                <div class="space-y-1">
+                  <!-- Current: "bg-primary-900 dark:bg-primary-600 text-white", Default: "text-primary-100 hover:bg-primary-800 dark:hover:bg-primary-600" -->
+                  <inertia-link
+                    v-for="link in props.mainNav"
+                    :href="link.route"
+                    class="group flex items-center px-2 py-2 text-base font-medium rounded-md transition"
+                    :class="{
+                      'bg-primary-900 dark:bg-primary-600 text-white': link.active,
+                      'text-gray-200 hover:text-gray-100 hover:bg-primary-800 dark:hover:bg-primary-600': !link.active,
+                    }"
+                  >
+                    <svg
+                      class="mr-3 h-6 w-6 transition"
+                      :class="{
+                        'text-primary-100': link.active,
+                        'text-primary-200 group-hover:text-primary-100': !link.active,
+                      }"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      aria-hidden="true"
+                      v-html="link.icon"
+                    />
+                    {{ link.label }}
+                  </inertia-link>
+                </div>
 
-                <a href="#" class="text-primary-100 hover:bg-primary-800 dark:hover:bg-primary-600 group flex items-center px-2 py-2 text-base font-medium rounded-md transition">
-                  <!-- Heroicon name: outline/users -->
-                  <svg class="text-primary-300 group-hover:text-primary-200 mr-4 h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z" />
-                  </svg>
-                  Team
-                </a>
-
-                <a href="#" class="text-primary-100 hover:bg-primary-600 group flex items-center px-2 py-2 text-base font-medium rounded-md">
-                  <!-- Heroicon name: outline/folder -->
-                  <svg class="mr-4 h-6 w-6 text-primary-300" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z" />
-                  </svg>
-                  Projects
-                </a>
-
-                <a href="#" class="text-primary-100 hover:bg-primary-600 group flex items-center px-2 py-2 text-base font-medium rounded-md">
-                  <!-- Heroicon name: outline/calendar -->
-                  <svg class="mr-4 h-6 w-6 text-primary-300" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
-                  </svg>
-                  Calendar
-                </a>
-
-                <a href="#" class="text-primary-100 hover:bg-primary-600 group flex items-center px-2 py-2 text-base font-medium rounded-md">
-                  <!-- Heroicon name: outline/inbox -->
-                  <svg class="mr-4 h-6 w-6 text-primary-300" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 13V6a2 2 0 00-2-2H6a2 2 0 00-2 2v7m16 0v5a2 2 0 01-2 2H6a2 2 0 01-2-2v-5m16 0h-2.586a1 1 0 00-.707.293l-2.414 2.414a1 1 0 01-.707.293h-3.172a1 1 0 01-.707-.293l-2.414-2.414A1 1 0 006.586 13H4" />
-                  </svg>
-                  Documents
-                </a>
-
-                <a href="#" class="text-primary-100 hover:bg-primary-600 group flex items-center px-2 py-2 text-base font-medium rounded-md">
-                  <!-- Heroicon name: outline/chart-bar -->
-                  <svg class="mr-4 h-6 w-6 text-primary-300" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
-                  </svg>
-                  Reports
-                </a>
+                <div class="space-y-1">
+                  <h3 class="px-3 text-xs font-semibold text-gray-200 uppercase tracking-wider" id="projects-headline">
+                    {{ __('Settings') }}
+                  </h3>
+                  <div class="space-y-1" role="group" aria-labelledby="projects-headline">
+                    <inertia-link
+                      v-for="link in props.subNav"
+                      :href="link.route"
+                      class="group flex items-center px-3 py-2 text-base font-medium rounded-md"
+                      :class="{
+                        'text-gray-50 bg-primary-900 dark:bg-primary-600': link.active,
+                        'text-gray-100 hover:text-gray-50 hover:bg-primary-700 dark:hover:bg-primary-600': !link.active,
+                      }"
+                    >
+                      <span class="truncate">
+                        {{ link.label }}
+                      </span>
+                    </inertia-link>
+                  </div>
+                </div>
               </nav>
             </div>
           </div>
@@ -122,14 +122,14 @@
                   class="group flex items-center px-2 py-2 text-sm font-medium rounded-md transition"
                   :class="{
                     'bg-primary-900 dark:bg-primary-600 text-white': link.active,
-                    'text-primary-100 hover:bg-primary-800 dark:hover:bg-primary-600': !link.active,
+                    'text-gray-200 hover:text-gray-100 hover:bg-primary-800 dark:hover:bg-primary-600': !link.active,
                   }"
                 >
                   <svg
                     class="mr-3 h-6 w-6 transition"
                     :class="{
                       'text-primary-100': link.active,
-                      'text-primary-300 group-hover:text-primary-200': !link.active,
+                      'text-primary-200 group-hover:text-primary-100': !link.active,
                     }"
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
@@ -152,8 +152,8 @@
                     :href="link.route"
                     class="group flex items-center px-3 py-2 text-sm font-medium rounded-md"
                     :class="{
-                      'text-gray-50 bg-primary-600': link.active,
-                      'text-gray-100 hover:text-gray-50 hover:bg-primary-600': !link.active,
+                      'text-gray-50 bg-primary-900 dark:bg-primary-600': link.active,
+                      'text-gray-100 hover:text-gray-50 hover:bg-primary-700 dark:hover:bg-primary-600': !link.active,
                     }"
                   >
                     <span class="truncate">
