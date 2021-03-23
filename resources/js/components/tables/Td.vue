e2d618a939342c24064e234c1f4e38d219951693
Grant Holle <hollegrant@gmail.com>
2021-03-23 10:29:54 +0800

    Improve table column display

--- resources/js/components/tables/Td.vue
+++ resources/js/components/tables/Td.vue
@@ -1,5 +1,24 @@
 <template>
-  <td class="px-6 py-4 text-sm font-medium">
+  <td
+    class="px-6 py-4 text-sm"
+    :class="{
+      'text-gray-500 dark:text-gray-300': lighter,
+      'text-gray-900 dark:text-gray-100 font-medium': !lighter,
+    }"
+  >
     <slot />
   </td>
 </template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {
+    lighter: {
+      type: Boolean,
+      default: true,
+    }
+  }
+})
+</script>
