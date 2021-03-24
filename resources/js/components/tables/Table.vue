72cb0400fe4e7ee725f3c92d0eed864e5d2ee15d
Grant Holle <hollegrant@gmail.com>
2021-03-24 09:20:22 +0800

    Remove bottom border from table

--- resources/js/components/tables/Table.vue
+++ resources/js/components/tables/Table.vue
@@ -2,7 +2,7 @@
   <div class="flex flex-col">
     <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
       <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-        <div class="shadow overflow-hidden border-b border-gray-200 dark:border-gray-600 sm:rounded-lg">
+        <div class="shadow overflow-hidden sm:rounded-lg">
           <table class="min-w-full divide-y divide-gray-200 dark:divide-gray-600">
             <slot />
           </table>
