550194b85ac53d0c0eed1fa65cda0419bd1cc036
Grant Holle <hollegrant@gmail.com>
2021-05-12 14:38:55 +0800

    Change layout

--- resources/js/components/slideouts/StudentInvoiceSlideout.vue
+++ resources/js/components/slideouts/StudentInvoiceSlideout.vue
@@ -86,10 +86,10 @@
           <InputWrap>
             <CheckboxWrapper>
               <Checkbox v-model:checked="form.notify" />
-              <CheckboxText>{{ __('Notify contacts of new invoice.') }}</CheckboxText>
+              <CheckboxText>{{ __('Send notification') }}</CheckboxText>
             </CheckboxWrapper>
             <HelpText>
-              {{ __('Having this option enabled will automatically send an email notifying contacts of the newly available invoice. There is a 15-minute delay of sending the notification which allows you to make adjustments, cancel the notification, or delete the invoice all together. If this is not enabled, you may send a notification manually later.') }}
+              {{ __("Having this option enabled will automatically queue an email to be sent notifying the appropriate parties of the available invoice. There is a 15-minute delay of sending the notification which allows you to make adjustments, cancel the notification, or delete the invoice all together. If this is not enabled, you may send a notification manually later.") }}
             </HelpText>
           </InputWrap>
         </Fieldset>
