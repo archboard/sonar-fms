d00eb12b18dee5083a317c8c4820e5e3013697f4
Grant Holle <hollegrant@gmail.com>
2021-05-13 09:12:12 +0800

    Add a loader component and implement on the invoice table

--- resources/js/components/StudentInvoiceTable.vue
+++ resources/js/components/StudentInvoiceTable.vue
@@ -1,77 +1,80 @@
 <template>
   <section>
-    <Table>
-      <Thead>
-        <tr>
-          <Th>{{ __('Title') }}</Th>
-          <Th>{{ __('Status') }}</Th>
-          <Th class="text-right">{{ __('Total') }}</Th>
-          <Th class="text-right">{{ __('Remaining') }}</Th>
-          <th/>
-        </tr>
-      </Thead>
-      <Tbody>
-        <tr
-          v-for="invoice in invoices.data"
-          :key="invoice.id"
-        >
-          <Td :lighter="false">
-            {{ invoice.title }}
-          </Td>
-          <Td>
-            <InvoiceStatusBadge :invoice="invoice" />
-          </Td>
-          <Td class="text-right">
-            {{ displayCurrency(invoice.amount_due) }}
-          </Td>
-          <Td class="text-right">
-            {{ displayCurrency(invoice.remaining_balance) }}
-          </Td>
-          <Td>
-            <div class="flex items-center justify-end space-x-2">
-              <Link is="button" class="text-sm" @click.prevent="$emit('edit', invoice)">
-                {{ __('Edit') }}
-              </Link>
-<!--              <VerticalDotMenu>-->
-<!--                <div class="px-1 py-1" v-if="canAny('viewAny', 'edit permissions')">-->
-<!--                  <SonarMenuItem v-if="can('viewAny')">-->
-<!--                    Edit-->
-<!--                  </SonarMenuItem>-->
-<!--                  <SonarMenuItem v-if="can('edit permissions')">-->
-<!--                    Permissions-->
-<!--                  </SonarMenuItem>-->
-<!--                </div>-->
-<!--                <div class="px-1 py-1">-->
-<!--                  <SonarMenuItem>-->
-<!--                    Archive-->
-<!--                  </SonarMenuItem>-->
-<!--                  <SonarMenuItem>-->
-<!--                    Move-->
-<!--                  </SonarMenuItem>-->
-<!--                </div>-->
+    <Loader v-if="loading" />
+    <div v-show="!loading">
+      <Table>
+        <Thead>
+          <tr>
+            <Th>{{ __('Title') }}</Th>
+            <Th>{{ __('Status') }}</Th>
+            <Th class="text-right">{{ __('Total') }}</Th>
+            <Th class="text-right">{{ __('Remaining') }}</Th>
+            <th/>
+          </tr>
+        </Thead>
+        <Tbody>
+          <tr
+            v-for="invoice in invoices.data"
+            :key="invoice.id"
+          >
+            <Td :lighter="false">
+              {{ invoice.title }}
+            </Td>
+            <Td>
+              <InvoiceStatusBadge :invoice="invoice" />
+            </Td>
+            <Td class="text-right">
+              {{ displayCurrency(invoice.amount_due) }}
+            </Td>
+            <Td class="text-right">
+              {{ displayCurrency(invoice.remaining_balance) }}
+            </Td>
+            <Td>
+              <div class="flex items-center justify-end space-x-2">
+                <Link is="button" class="text-sm" @click.prevent="$emit('edit', invoice)">
+                  {{ __('Edit') }}
+                </Link>
+  <!--              <VerticalDotMenu>-->
+  <!--                <div class="px-1 py-1" v-if="canAny('viewAny', 'edit permissions')">-->
+  <!--                  <SonarMenuItem v-if="can('viewAny')">-->
+  <!--                    Edit-->
+  <!--                  </SonarMenuItem>-->
+  <!--                  <SonarMenuItem v-if="can('edit permissions')">-->
+  <!--                    Permissions-->
+  <!--                  </SonarMenuItem>-->
+  <!--                </div>-->
+  <!--                <div class="px-1 py-1">-->
+  <!--                  <SonarMenuItem>-->
+  <!--                    Archive-->
+  <!--                  </SonarMenuItem>-->
+  <!--                  <SonarMenuItem>-->
+  <!--                    Move-->
+  <!--                  </SonarMenuItem>-->
+  <!--                </div>-->
 
-<!--                <div class="px-1 py-1" v-if="can('delete')">-->
-<!--                  <SonarMenuItem v-slot="{ active }">-->
-<!--                    <span :class="[active ? '' : 'text-red-500 dark:text-red-400']">Delete</span>-->
-<!--                  </SonarMenuItem>-->
-<!--                </div>-->
-<!--              </VerticalDotMenu>-->
-            </div>
-          </Td>
-        </tr>
-        <tr v-if="invoices.data.length === 0">
-          <Td colspan="5" class="text-center">
-            {{ __('No invoices exist for this student.') }}
-          </Td>
-        </tr>
-      </Tbody>
-    </Table>
+  <!--                <div class="px-1 py-1" v-if="can('delete')">-->
+  <!--                  <SonarMenuItem v-slot="{ active }">-->
+  <!--                    <span :class="[active ? '' : 'text-red-500 dark:text-red-400']">Delete</span>-->
+  <!--                  </SonarMenuItem>-->
+  <!--                </div>-->
+  <!--              </VerticalDotMenu>-->
+              </div>
+            </Td>
+          </tr>
+          <tr v-if="invoices.data.length === 0">
+            <Td colspan="5" class="text-center">
+              {{ __('No invoices exist for this student.') }}
+            </Td>
+          </tr>
+        </Tbody>
+      </Table>
 
-    <div :class="{ 'py-6': invoices.data.length > 0 }">
-      <Pagination
-        :meta="invoices.meta"
-        @paged="paged"
-      />
+      <div :class="{ 'py-6': invoices.data.length > 0 }">
+        <Pagination
+          :meta="invoices.meta"
+          @paged="paged"
+        />
+      </div>
     </div>
   </section>
 </template>
@@ -86,9 +89,11 @@ import VerticalDotMenu from './dropdown/VerticalDotMenu'
 import SonarMenuItem from './forms/SonarMenuItem'
 import checksPermissions from '../composition/checksPermissions'
 import Link from './Link'
+import Loader from './Loader'
 
 export default defineComponent({
   components: {
+    Loader,
     VerticalDotMenu,
     InvoiceStatusBadge,
     ...TableComponents,
@@ -105,6 +110,7 @@ export default defineComponent({
   setup (props) {
     const $route = inject('$route')
     const $http = inject('$http')
+    const loading = ref(true)
     const { can, canAny } = checksPermissions(props.permissions || {})
     const filters = reactive({
       page: 1,
@@ -117,6 +123,7 @@ export default defineComponent({
       links: {}
     })
     const fetchInvoices = () => {
+      loading.value = true
       const params = {
         ...filters,
         student: props.student,
@@ -125,6 +132,7 @@ export default defineComponent({
 
       $http.get(route).then(({ data }) => {
         invoices.value = data
+        loading.value = false
       })
     }
     const paged = page => {
@@ -145,6 +153,7 @@ export default defineComponent({
       filters,
       paged,
       fetchInvoices,
+      loading,
     }
   }
 })
