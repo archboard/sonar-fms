ce1ba29f8eb2bb3f9047e228c4f0580a1361ce24
Grant Holle <hollegrant@gmail.com>
2021-06-07 15:48:22 +0800

    Add a mocker component that wraps an element to hide it but maintain the same size

---
+++ resources/js/components/Mocker.vue
@@ -0,0 +1,35 @@
+<template>
+  <div
+    class="relative"
+    :class="{
+      'animate-pulse': pulse,
+      'opacity-25': !pulse,
+    }"
+  >
+    <div class="absolute rounded inset-0 bg-gradient-to-br from-gray-300 to-gray-200 dark:from-gray-700 dark:to-gray-600"></div>
+    <div class="invisible">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  props: {
+    mock: {
+      type: Boolean,
+      default: false,
+    },
+    inline: {
+      type: Boolean,
+      default: false,
+    },
+    pulse: {
+      type: Boolean,
+      default: false,
+    },
+  }
+})
+</script>
