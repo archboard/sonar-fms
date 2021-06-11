876f49b5e8fd3298218aca587b5642f2a1a72d5b
Grant Holle <hollegrant@gmail.com>
2021-06-11 09:33:41 +0800

    Refactor currency display to be more readable

--- resources/js/composition/displaysCurrency.js
+++ resources/js/composition/displaysCurrency.js
@@ -3,11 +3,16 @@ import { usePage } from '@inertiajs/inertia-vue3'
 export default () => {
   const { props } = usePage()
   const currency = props.value.school?.currency || { code: 'USD' }
-  const locale = props.value.user?.locale
+  const locale = props.value.user?.locale || 'en'
   const displayCurrency = amount => {
     const asFloat = (amount || 0) / Math.pow(10, (currency?.digits || 2))
+    const options = {
+      style: 'currency',
+      currency: currency.code
+    }
 
-    return new Intl.NumberFormat(locale || 'en', { style: 'currency', currency: currency.code }).format(asFloat)
+    return new Intl.NumberFormat(locale, options)
+      .format(asFloat)
   }
 
   return {
