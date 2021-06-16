10679713e31e15f96ec95c323bfc4906c0108fa7
Grant Holle <hollegrant@gmail.com>
2021-06-16 10:16:10 +0800

    Replace drop down component to something simple

--- resources/js/components/forms/Dropdown.vue
+++ resources/js/components/forms/Dropdown.vue
@@ -1,84 +1,55 @@
 <template>
-  <div class="relative">
-    <div @click="open = ! open">
-      <slot name="trigger" />
-    </div>
-
-    <!-- Full Screen Dropdown Overlay -->
-    <div v-show="open" class="fixed inset-0 z-40" @click="open = false">
+  <Menu as="div" class="relative inline-block text-left z-10">
+    <div>
+      <MenuButton class="focus:outline-none">
+        <Button>
+          <slot />
+          <ChevronDownIcon class="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
+        </Button>
+      </MenuButton>
     </div>
 
     <transition
-      enter-active-class="transition ease-out duration-200"
+      enter-active-class="transition ease-out duration-100"
       enter-from-class="transform opacity-0 scale-95"
       enter-to-class="transform opacity-100 scale-100"
       leave-active-class="transition ease-in duration-75"
       leave-from-class="transform opacity-100 scale-100"
-      leave-to-class="transform opacity-0 scale-95">
-      <div
-        v-show="open"
-        class="absolute z-50 mt-2 rounded-md shadow-lg"
-        :class="[widthClass, alignmentClasses]"
-        style="display: none;"
-        @click="open = false"
-      >
-        <div class="rounded-md ring-1 ring-black ring-opacity-5" :class="contentClasses">
-          <slot name="content" />
+      leave-to-class="transform opacity-0 scale-95"
+    >
+      <MenuItems class="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white dark:bg-gray-700 ring-1 ring-black ring-opacity-5 focus:outline-none">
+        <div class="py-1">
+          <MenuItem
+            v-slot="{ active }"
+            v-for="item in menuItems"
+          >
+            <component :is="item.component || 'inertia-link'" :href="item.route" :class="[active ? 'bg-gray-100 text-gray-900 dark:bg-gray-800 dark:text-white' : 'text-gray-700 dark:text-gray-100', 'block px-4 py-2 text-sm']">
+              {{ item.label }}
+            </component>
+          </MenuItem>
         </div>
-      </div>
+      </MenuItems>
     </transition>
-  </div>
+  </Menu>
 </template>
 
 <script>
-import { onMounted, onUnmounted, ref } from 'vue'
+import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
+import { ChevronDownIcon } from '@heroicons/vue/solid'
+import Button from '../Button'
 
 export default {
+  components: {
+    Button,
+    Menu,
+    MenuButton,
+    MenuItem,
+    MenuItems,
+    ChevronDownIcon,
+  },
+
   props: {
-    align: {
-      default: 'right'
-    },
-    width: {
-      default: '48'
-    },
-    contentClasses: {
-      default: () => ['py-1', 'bg-white']
-    }
-  },
-
-  setup() {
-    let open = ref(false)
-
-    const closeOnEscape = (e) => {
-      if (open.value && e.keyCode === 27) {
-        open.value = false
-      }
-    }
-
-    onMounted(() => document.addEventListener('keydown', closeOnEscape))
-    onUnmounted(() => document.removeEventListener('keydown', closeOnEscape))
-
-    return {
-      open,
-    }
-  },
-
-  computed: {
-    widthClass() {
-      return {
-        '48': 'w-48',
-      }[this.width.toString()]
-    },
-
-    alignmentClasses() {
-      if (this.align === 'left') {
-        return 'origin-top-left left-0'
-      } else if (this.align === 'right') {
-        return 'origin-top-right right-0'
-      } else {
-        return 'origin-top'
-      }
-    },
+    menuItems: Array,
   }
 }
 </script>
