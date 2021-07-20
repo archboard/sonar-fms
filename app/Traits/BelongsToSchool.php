a99de5bcd724ad7bd84b0232958ded9c785232a5
Grant Holle <hollegrant@gmail.com>
2021-07-20 10:47:18 +0800

    Add currency relation to those that belong to school

--- app/Traits/BelongsToSchool.php
+++ app/Traits/BelongsToSchool.php
@@ -2,13 +2,27 @@
 
 namespace App\Traits;
 
+use App\Models\Currency;
 use App\Models\School;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOneThrough;
 
 trait BelongsToSchool
 {
     public function school(): BelongsTo
     {
         return $this->belongsTo(School::class);
+    }
+
+    public function currency(): HasOneThrough
+    {
+        return $this->hasOneThrough(
+            Currency::class,
+            School::class,
+            'id',
+            'id',
+            'school_id',
+            'currency_id'
+        );
     }
 }
