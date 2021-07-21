4443007070c1f08e888a907faeecc6f55e96b8e5
Grant Holle <hollegrant@gmail.com>
2021-07-21 09:47:13 +0800

    Stop inheriting attributes for the sonar menu item

--- resources/js/components/forms/SonarMenuItem.vue
+++ resources/js/components/forms/SonarMenuItem.vue
@@ -6,7 +6,7 @@
         active ? 'bg-gradient-to-r from-fuchsia-500 to-fuchsia-600 dark:from-fuchsia-600 dark:to-fuchsia-700 text-white' : 'text-gray-900 dark:text-gray-100',
         'group flex rounded-md items-center w-full px-2 py-2 text-sm focus:outline-none focus:ring-1 focus:ring-gray-300 dark:focus:ring-gray-500',
       ]"
-      @click.prevent="$emit('click')"
+      v-bind="$attrs"
     >
       <slot :active="active" />
     </component>
@@ -18,10 +18,10 @@ import { defineComponent } from 'vue'
 import { MenuItem } from '@headlessui/vue'
 
 export default defineComponent({
+  inheritAttrs: false,
   components: {
     MenuItem,
   },
-  emits: ['click'],
 
   props: {
     is: {
