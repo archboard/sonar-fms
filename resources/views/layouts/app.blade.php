9cd56034ae8c121a393c4b8502c406c1d2da415e
Grant Holle <hollegrant@gmail.com>
2021-07-27 15:47:30 +0800

    Add robot meta tag

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
   <head>
+    <meta name="robots" content="noindex,nofollow">
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0" />
     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
