b85ce4b113458a312a0cd61a92469c04dbe9caf5
Grant Holle <hollegrant@gmail.com>
2021-08-02 09:41:49 +0800

    Use page's permission property first

--- resources/js/composition/checksPermissions.js
+++ resources/js/composition/checksPermissions.js
@@ -1,8 +1,14 @@
 import get from 'lodash/get'
+import { usePage } from '@inertiajs/inertia-vue3'
+import { computed } from 'vue'
 
-export default (permissions) => {
-  const can = (...perms) => perms.every(perm => get(permissions, perm))
-  const canAny = (...perms) => perms.some(perm => get(permissions, perm))
+export default (permissions = {}) => {
+  const page = usePage()
+  const localPermissions = computed(
+    () => page.props.value.permissions || permissions
+  )
+  const can = (...perms) => perms.every(perm => get(localPermissions.value, perm))
+  const canAny = (...perms) => perms.some(perm => get(localPermissions.value, perm))
 
   return {
     can,
