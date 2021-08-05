7d5bb2b8c78ceb81f0a87cff1841d786209dc05f
Grant Holle <hollegrant@gmail.com>
2021-08-05 10:45:42 +0800

    Style the error page

--- resources/js/pages/Error.vue
+++ resources/js/pages/Error.vue
@@ -1,14 +1,40 @@
 <template>
-  <div>
-    <h1>{{ title }}</h1>
-    <div>{{ description }}</div>
+  <div class="error-bg min-h-screen w-full flex items-center justify-center">
+    <div class="max-w-lg w-full mx-auto px-4">
+      <CardWrapper>
+        <CardPadding class="space-y-4 text-center">
+          <h1 class="font-medium text-2xl">{{ title }}</h1>
+          <div>{{ description }}</div>
+          <div class="space-y-4 md:flex md:space-x-5 md:space-y-0">
+            <Button component="inertia-link" href="/" is-block>
+              {{ __('Home') }}
+            </Button>
+            <Button component="a" href="https://www.archboard.io/products/sonar-fms/" is-block target="_blank">
+              {{ __('About Sonar FMS') }}
+            </Button>
+          </div>
+        </CardPadding>
+      </CardWrapper>
+    </div>
   </div>
 </template>
 
 <script>
+import PageProps from '@/mixins/PageProps'
+import setsTitle from '@/composition/setsTitle'
+import CardWrapper from '@/components/CardWrapper'
+import CardPadding from '@/components/CardPadding'
+import Button from '@/components/Button'
+
 export default {
+  components: {Button, CardPadding, CardWrapper},
+  mixins: [PageProps],
   props: {
     status: Number,
+  },
+
+  setup () {
+    setsTitle()
   },
 
   computed: {
@@ -18,7 +44,7 @@ export default {
         500: this.__('500: Server Error'),
         404: this.__('404: Page Not Found'),
         403: this.__('403: Forbidden'),
-      }[this.status]
+      }[this.status] || 'Error'
     },
 
     description () {
