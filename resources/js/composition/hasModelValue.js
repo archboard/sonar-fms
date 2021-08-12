005d09a5edd0c2138fccc3242091bbd8219615aa
Grant Holle <hollegrant@gmail.com>
2021-08-12 10:15:41 +0800

    Use object getter/setting for computed value

--- resources/js/composition/hasModelValue.js
+++ resources/js/composition/hasModelValue.js
@@ -1,12 +1,12 @@
 import { computed } from 'vue'
 
 export default (props, emit) => {
-  const localValue = computed(
-      () => props.modelValue,
-      state => emit('update:modelValue', state)
-    )
+  const localValue = computed({
+    get: () => props.modelValue,
+    set: state => emit('update:modelValue', state)
+  })
 
-    return {
-      localValue
-    }
+  return {
+    localValue
+  }
 }
