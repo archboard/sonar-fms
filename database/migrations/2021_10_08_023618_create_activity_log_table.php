456c776487d4143f65904213e20d4eecf637c847
Grant Holle <hollegrant@gmail.com>
2021-10-11 09:20:06 +0800

    Adjust activity log migration to accommodate uuids and ids

--- database/migrations/2021_10_08_023618_create_activity_log_table.php
+++ database/migrations/2021_10_08_023618_create_activity_log_table.php
@@ -12,7 +12,9 @@ class CreateActivityLogTable extends Migration
             $table->bigIncrements('id');
             $table->string('log_name')->nullable();
             $table->text('description');
-            $table->nullableMorphs('subject', 'subject');
+            $table->string('subject_type')->nullable()->index();
+            // This could be an int ID or a UUID
+            $table->string('subject_id')->nullable()->index();
             $table->nullableMorphs('causer', 'causer');
             $table->json('properties')->nullable();
             $table->timestamps();
