0243eb7a0fbb60bdf83307f8d22550a94f61f546
Grant Holle <hollegrant@gmail.com>
2021-11-02 10:35:27 +0800

    Add a students has many through relationship for invoices

--- app/Models/Invoice.php
+++ app/Models/Invoice.php
@@ -20,6 +20,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 use Illuminate\Database\Eloquent\Relations\HasOneThrough;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Arr;
@@ -250,6 +251,18 @@ class Invoice extends Model
     public function users(): BelongsToMany
     {
         return $this->belongsToMany(User::class);
+    }
+
+    public function students(): HasManyThrough
+    {
+        return $this->hasManyThrough(
+            Student::class,
+            static::class,
+            'parent_uuid',
+            'uuid',
+            'uuid',
+            'student_uuid'
+        );
     }
 
     public function getIsVoidAttribute(): bool
