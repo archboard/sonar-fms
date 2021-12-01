0e55c5b53a55fd173def142c71f846f88c43d923
Grant Holle <hollegrant@gmail.com>
2021-12-01 14:23:18 +0800

    Use `viewAny` ability for the show resource action

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,22 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * Get the map of resource methods to ability names.
+     *
+     * @return array
+     */
+    protected function resourceAbilityMap(): array
+    {
+        return [
+            'index' => 'viewAny',
+            'show' => 'viewAny',
+            'create' => 'create',
+            'store' => 'create',
+            'edit' => 'update',
+            'update' => 'update',
+            'destroy' => 'delete',
+        ];
+    }
 }
