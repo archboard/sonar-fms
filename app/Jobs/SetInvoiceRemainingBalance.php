530c28e4e555af3eede740d54dfc990703fcbd81
Grant Holle <hollegrant@gmail.com>
2021-12-03 09:50:58 +0800

    Add a job for calculating remaining balances

---
+++ app/Jobs/SetInvoiceRemainingBalance.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Invoice;
+use Illuminate\Bus\Batchable;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class SetInvoiceRemainingBalance implements ShouldQueue
+{
+    use Batchable, Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(protected string $invoiceUuid)
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        if ($this->batch()->cancelled()) {
+            return;
+        }
+
+        Invoice::find($this->invoiceUuid)
+            ->setRemainingBalance()
+            ->save();
+    }
+}
