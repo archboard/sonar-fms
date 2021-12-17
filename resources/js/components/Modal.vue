96c8cc2697abd5c8fb3c6740345a9f300a15ee59
Grant Holle <hollegrant@gmail.com>
2021-12-17 10:53:20 +0800

    Add action loading to button

--- resources/js/components/Modal.vue
+++ resources/js/components/Modal.vue
@@ -22,7 +22,7 @@
         </div>
         <div class="bg-gray-50 dark:bg-gray-700 px-4 py-3 sm:px-6 flex flex-col space-y-2 sm:space-y-0 sm:flex-row-reverse rounded-b-2xl">
           <slot name="actions">
-            <Button @click.prevent="performAction" type="button" :color="actionColor" class="sm:ml-2 text-sm" ref="action">
+            <Button @click.prevent="performAction" type="button" :loading="actionLoading" :color="actionColor" class="sm:ml-2 text-sm" ref="action">
               {{ computedActionText }}
             </Button>
             <Button @click.prevent="close" type="button" color="white" class="text-sm">
