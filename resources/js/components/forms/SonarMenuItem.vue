fc690c73934cb21b4fa923e905898e1d1dfd9a1a
Grant Holle <hollegrant@gmail.com>
2021-12-27 12:23:10 +0800

    Add cursor styles

--- resources/js/components/forms/SonarMenuItem.vue
+++ resources/js/components/forms/SonarMenuItem.vue
@@ -4,7 +4,7 @@
       :is="is"
       :class="[
         active ? 'bg-gradient-to-bl from-fuchsia-500 to-fuchsia-600 dark:from-fuchsia-600 dark:to-fuchsia-700 text-white' : 'text-gray-900 dark:text-gray-100',
-        'group flex rounded-md items-center w-full px-2 py-2 text-sm focus:outline-none focus:ring-1 focus:ring-gray-300 dark:focus:ring-gray-500',
+        'group flex rounded-md items-center w-full px-2 py-2 text-sm cursor-pointer focus:outline-none focus:ring-1 focus:ring-gray-300 dark:focus:ring-gray-500',
       ]"
       v-bind="$attrs"
     >
