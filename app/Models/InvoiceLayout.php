7789b1c8828cbdf5637a8ddebcf0eefc96a37af9
Grant Holle <hollegrant@gmail.com>
2022-01-28 15:19:46 +0800

    Abstract layout base class

--- app/Models/InvoiceLayout.php
+++ app/Models/InvoiceLayout.php
@@ -2,60 +2,15 @@
 
 namespace App\Models;
 
-use App\Traits\BelongsToSchool;
-use App\Traits\BelongsToTenant;
-use GrantHolle\Http\Resources\Traits\HasResource;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 /**
  * @mixin IdeHelperInvoiceLayout
  */
-class InvoiceLayout extends Model
+class InvoiceLayout extends LayoutBase
 {
-    use HasFactory;
-    use HasResource;
-    use BelongsToTenant;
-    use BelongsToSchool;
-
-    protected $guarded = [];
-
-    protected $casts = [
-        'layout_data' => 'json',
-    ];
-
-    public function scopeFilter(Builder $builder, array $filters)
-    {
-        $builder->when($filters['s'] ?? null, function (Builder $builder, string $search) {
-            $builder->where('name', 'ilike', "%{$search}%");
-        });
-
-        $orderBy = $filters['orderBy'] ?? 'name';
-        $orderDir = $filters['orderDir'] ?? 'asc';
-
-        $builder->orderBy($orderBy, $orderDir);
-        $builder->orderBy('name', $orderDir);
-    }
-
-    public function scopeDefault(Builder $builder, bool $status = true)
-    {
-        $builder->where('is_default', $status);
-    }
-
     public function invoices(): HasMany
     {
         return $this->hasMany(Invoice::class);
-    }
-
-    public function getMaxWidthAttribute(): string
-    {
-        $pages = [
-            'Letter' => '8.5in',
-            'A4' => '8.27in',
-        ];
-
-        return $pages[$this->paper_size];
     }
 }
---
+++ app/Models/LayoutBase.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\BelongsToSchool;
+use App\Traits\BelongsToTenant;
+use GrantHolle\Http\Resources\Traits\HasResource;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class LayoutBase extends Model
+{
+    use HasFactory;
+    use HasResource;
+    use BelongsToTenant;
+    use BelongsToSchool;
+
+    protected $guarded = [];
+
+    protected $casts = [
+        'layout_data' => 'json',
+        'is_default' => 'boolean',
+    ];
+
+    public function scopeFilter(Builder $builder, array $filters)
+    {
+        $builder->when($filters['s'] ?? null, function (Builder $builder, string $search) {
+            $builder->where('name', 'ilike', "%{$search}%");
+        });
+
+        $orderBy = $filters['orderBy'] ?? 'name';
+        $orderDir = $filters['orderDir'] ?? 'asc';
+
+        $builder->orderBy($orderBy, $orderDir);
+        $builder->orderBy('name', $orderDir);
+    }
+
+    public function scopeDefault(Builder $builder, bool $status = true)
+    {
+        $builder->where('is_default', $status);
+    }
+
+    public function getMaxWidthAttribute(): string
+    {
+        $pages = [
+            'Letter' => '8.5in',
+            'A4' => '8.27in',
+        ];
+
+        return $pages[$this->paper_size];
+    }
+}
