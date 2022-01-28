15750e95753ef89a7d37101dbfd488f92106965a
Grant Holle <hollegrant@gmail.com>
2022-01-28 09:48:17 +0800

    Change breakpoint

--- resources/js/components/TwoColumnWrapper.vue
+++ resources/js/components/TwoColumnWrapper.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="grid gap-6 sm:grid-cols-2 md:grid-cols-1 xl:grid-cols-2">
+  <div class="grid gap-6 sm:grid-cols-2 md:grid-cols-1 lg:grid-cols-2">
     <slot />
   </div>
 </template>
