38bdf2c78646936bda48f7d52ef2456cc7fb9c5e
Grant Holle <hollegrant@gmail.com>
2022-02-16 15:00:19 +0800

    Add custom cast with Laravel 9 syntax

--- app/Traits/HasAmountAttribute.php
+++ app/Traits/HasAmountAttribute.php
@@ -3,16 +3,19 @@
 namespace App\Traits;
 
 use Brick\Money\Money;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 trait HasAmountAttribute
 {
-    public function getAmountFormattedAttribute(): string
+    public function amountFormatted(): Attribute
     {
-        if ($this->relationLoaded('currency')) {
-            return Money::ofMinor($this->amount, $this->currency->code)
-                ->formatTo(auth()->user()->locale ?? 'en');
-        }
+        return Attribute::get(function (): string {
+            if ($this->relationLoaded('currency')) {
+                return Money::ofMinor($this->amount, $this->currency->code)
+                    ->formatTo(auth()->user()?->locale ?? 'en');
+            }
 
-        return '';
+            return '';
+        });
     }
 }
