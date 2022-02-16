f6a6f82c6db889e1d4a17450543b10c62b042aef
Grant Holle <hollegrant@gmail.com>
2022-02-16 10:59:02 +0800

    Add offset option to display function

--- resources/js/composition/displaysDate.js
+++ resources/js/composition/displaysDate.js
@@ -16,7 +16,7 @@ export default () => {
   }
 
   const getDate = (date, offset = false) => (date ? dayjs(date) : dayjs()).tz(timezone.value, offset)
-  const displayDate = (date, format) => getDate(date).format(formats[format] || format)
+  const displayDate = (date, format, offset = false) => getDate(date, offset).format(formats[format] || format)
   const fromNow = (date) => getDate(date).fromNow()
 
   return {
