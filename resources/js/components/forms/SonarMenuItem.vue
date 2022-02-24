c69f1540192588ee8289406d8cd79df1c055411c
Grant Holle <hollegrant@gmail.com>
2022-02-24 13:51:34 +0800

    Abstract classes for menu items

--- resources/js/components/forms/SonarMenuItem.vue
+++ resources/js/components/forms/SonarMenuItem.vue
@@ -3,8 +3,8 @@
     <component
       :is="is"
       :class="[
-        active ? 'bg-gradient-to-bl from-fuchsia-500 to-fuchsia-600 dark:from-fuchsia-600 dark:to-fuchsia-700 text-white' : 'text-gray-900 dark:text-gray-100',
-        'group flex rounded-md items-center w-full px-2 py-2 text-sm cursor-pointer focus:outline-none focus:ring-1 focus:ring-gray-300 dark:focus:ring-gray-500',
+        active ? classes.active : classes.inactive,
+        classes.always,
       ]"
       v-bind="$attrs"
     >
@@ -16,6 +16,7 @@
 <script>
 import { defineComponent } from 'vue'
 import { MenuItem } from '@headlessui/vue'
+import menuItemClasses from '@/composition/menuItemClasses'
 
 export default defineComponent({
   inheritAttrs: false,
@@ -29,5 +30,13 @@ export default defineComponent({
       default: 'button',
     }
   },
+
+  setup () {
+    const classes = menuItemClasses()
+
+    return {
+      classes,
+    }
+  }
 })
 </script>
