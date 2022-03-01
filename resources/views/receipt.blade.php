e9188a9193ea97ed0ef8703a5b1670b6a8d2f5bf
Grant Holle <hollegrant@gmail.com>
2022-03-01 13:16:26 +0800

    Fix display

--- resources/views/receipt.blade.php
+++ resources/views/receipt.blade.php
@@ -82,7 +82,7 @@
               <div class="py-4 sm:grid sm:py-5 sm:grid-cols-3 sm:gap-4">
                 <dt class="text-sm font-medium text-gray-500">{{ __('Payment amount') }}</dt>
                 <dd class="mt-1 flex text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                  <span class="flex-grow">{{ $payment->amount_formatted }}</span>
+                  <span class="flex-grow">{{ displayCurrency($payment->amount, $currency) }}</span>
                 </dd>
               </div>
               <div class="py-4 sm:grid sm:py-5 sm:grid-cols-3 sm:gap-4">
