e0c8c895513c23273ddfd5a2b628786be9289689
Grant Holle <hollegrant@gmail.com>
2022-03-03 13:40:43 +0800

    Use abstracted functionality

--- app/Listeners/SetUserSchool.php
+++ app/Listeners/SetUserSchool.php
@@ -17,18 +17,7 @@ class SetUserSchool
     {
         /** @var User $user */
         $user = $event->user;
-
-        if (
-            !$user->school_id ||
-            !$user->schools->contains('id', $user->school_id)
-        ) {
-            $school = $user->schools->first();
-
-            if (!$school) {
-                throw new \Exception("You do not have any schools configured for your account. Please contact your district admin for help.");
-            }
-
-            $user->update(['school_id' => $school->id]);
-        }
+        $user->setSchool()
+            ->save();
     }
 }
