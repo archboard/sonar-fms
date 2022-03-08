33699234fa9122f9a3bbbb41ca0854941df61866
Grant Holle <hollegrant@gmail.com>
2022-03-08 11:42:54 +0800

    Scope the global search to the current school

--- app/Http/Controllers/GlobalSearchController.php
+++ app/Http/Controllers/GlobalSearchController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Fee;
 use App\Models\Invoice;
 use App\Models\Scholarship;
+use App\Models\School;
 use App\Models\Student;
 use Illuminate\Http\Request;
 use Spatie\Searchable\ModelSearchAspect;
@@ -19,27 +20,31 @@ class GlobalSearchController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\JsonResponse
      */
-    public function __invoke(Request $request)
+    public function __invoke(Request $request, School $school)
     {
         $results = (new Search())
-            ->registerModel(Invoice::class, function (ModelSearchAspect $aspect) {
+            ->registerModel(Invoice::class, function (ModelSearchAspect $aspect) use ($school) {
                 $aspect->addSearchableAttribute('invoice_number')
                     ->addSearchableAttribute('title')
+                    ->where('school_id', $school->id)
                     ->with('student', 'students', 'currency');
             })
-            ->registerModel(Student::class, function (ModelSearchAspect $aspect) {
+            ->registerModel(Student::class, function (ModelSearchAspect $aspect) use ($school) {
                 $aspect->addSearchableAttribute('first_name')
                     ->addSearchableAttribute('last_name')
                     ->addSearchableAttribute('student_number')
+                    ->where('school_id', $school->id)
+                    ->orderBy('enrolled', 'desc')
                     ->orderBy('last_name')
                     ->orderBy('first_name');
             })
-            ->registerModel(Fee::class, function (ModelSearchAspect $aspect) {
+            ->registerModel(Fee::class, function (ModelSearchAspect $aspect) use ($school) {
                 $aspect->addSearchableAttribute('name')
                     ->with('currency');
             })
-            ->registerModel(Scholarship::class, function (ModelSearchAspect $aspect) {
+            ->registerModel(Scholarship::class, function (ModelSearchAspect $aspect) use ($school) {
                 $aspect->addSearchableAttribute('name')
+                    ->where('school_id', $school->id)
                     ->with('currency');
             })
             ->limitAspectResults(10)
