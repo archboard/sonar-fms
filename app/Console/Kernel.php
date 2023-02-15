1e76c25901ba445773174b78df26f1c33b61cf80
Grant Holle <hollegrant@gmail.com>
2023-02-15 14:54:49 +0400

    Start 10 upgrade, waiting on geo-ip package to update :(

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,6 +26,8 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
          $schedule->command(SyncTenantSisData::class)->hourly();
+
+         $schedule->command('cache:prune-stale-tags')->hourly();
     }
 
     /**
